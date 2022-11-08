73db69274564e0cff2e03a0ad194b8bd97211938
Martin <martinmongi95@gmail.com>
2022-11-08 15:51:27 -0300

    5:Chatbox

--- Java/Java Bootcamp/Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/2. Variables/Workbook 2.4/Meeting.java
+++ Java/Java Bootcamp/Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/2. Variables/Workbook 2.4/Meeting.java
@@ -4,14 +4,18 @@ public class Meeting {
         // Instructions for this workbook are on Learn the Part (see the Udemy Video: Workbook 2.4 to access the link).
 
         double sales = 24309.65; 
+        int salesInInt = (int)sales;
         double profit = 18562.18; 
+        int profitInInt = (int)profit;
         double refunds = 688.78; 
+        int refundsInInt =(int)refunds;
         double shipping = 1233.57; 
+        int shippingInInt=(int)shipping;
 
-        System.out.println("This month, we made $<sales> in sales");
-        System.out.println("Factoring in costs, we made $<profit> in profit");
-        System.out.println("The refunds are at a low $<refunds>. This is a good sign!");
-        System.out.println("Shipping costs were high. We paid $<shipping> in shipping");
+        System.out.println("This month, we made $"+ salesInInt + " in sales");
+        System.out.println("Factoring in costs, we made $" + profitInInt + " in profit");
+        System.out.println("The refunds are at a low $"+ refundsInInt + ". This is a good sign!");
+        System.out.println("Shipping costs were high. We paid $" + shippingInInt + " in shipping");
 
 
     }
